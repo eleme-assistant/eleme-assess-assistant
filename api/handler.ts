dfd64b637add944ef0d402f27ef7e358facafdc1
Zhou Yu <muyu.zhouyu@outlook.com>
2017-12-30 12:09:52 +0800

    update: 数据请求

--- api/handler.ts
+++ api/handler.ts
@@ -1,15 +1,34 @@
 import { Eleme } from '../../interface/eleme_assess_assistant';
 import * as _ from 'lodash';
+import axios, { AxiosResponse } from 'axios';
 
 export class Handler {
 
     /**
      * 所有需要处理的店铺原始评分数据
      */
-    private readonly items: Eleme.RatingScore[];
+    private readonly items: Eleme.RatingScore[] = [];
+    private readonly restaurant_ids: number[];
 
-    constructor(items: Eleme.RatingScore[]) {
-        this.items = items;
+    constructor(restaurant_ids: number[]) {
+        this.restaurant_ids = restaurant_ids;
+    }
+
+    public async init() {
+        for (const restaurant_id of this.restaurant_ids) {
+            const data = await this.requestRestaurant(restaurant_id);
+            if (data && !_.isEmpty(data)) this.items.push(data);
+        }
+    }
+
+    public async requestRestaurant(restaurant_id: number) {
+        try {
+            const response = await axios.get(`https://www.ele.me/restapi/ugc/v1/restaurants/${restaurant_id}/rating_scores?latitude=0&longitude=0`);
+            return <Eleme.RatingScore>response.data;
+        } catch (e) {
+            if (e.status === 404) return false;
+            throw Error('服务请求失败');
+        }
     }
 
     public resolve(): number[] {
--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
 import { Handler } from './api/handler';
 import * as _ from 'lodash';
 
-export function main(event, context, callback) {
+export async function main(event, context) {
     let error = null;
 
     const handler = new Handler(event);
+    await handler.init();
     const res = handler.resolve();
 
     if (_.takeWhile(res, _.isNumber).length !== res.length) error = Error('数据异常');
 
-    return callback(error, res);
+    return error || res;
 }
